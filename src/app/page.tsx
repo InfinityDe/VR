6ca4035a75d7b4b05eae87dd36760d2600d5bdae
InfinityDe <infinity.des.dev@gmail.com>
2024-02-20 11:44:02 -0300

    metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 "use client"
 import { Introduction } from '@/components/Introduction'
-import { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'VR',
-  description: 'VR',
-}
-
 
 export default function Home() {
   return (
